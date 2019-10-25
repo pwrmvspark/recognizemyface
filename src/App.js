1fb84ee6baccba88ebb13728ffbee9936bd71c17
pwrmvspark <danielparkonline@gmail.com>
2019-10-25 15:19:22 -0700

    FaceRecognition component finalized, Signin component started

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import Logo from './components/Logo/Logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import Rank from './components/Rank/Rank.js'
+import Signin from './components/Signin/Signin.js'
 import './App.css';
 
 const app = new Clarifai.App({
@@ -29,8 +30,29 @@ class App extends Component {
     super() 
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route: 'signin'
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height )
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box})
   }
 
   onInputChange = (e) => {
@@ -43,14 +65,8 @@ class App extends Component {
     .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        // there was an error
-      }
-  );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(error => console.log(error))
   }
 
   render() {
@@ -60,13 +76,14 @@ class App extends Component {
           params={particleOptions} 
         />
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect facee in your pictures. Git it a try =).'}
+        {'This Magic Brain will detect face(s) in your pictures. Git it a try =).'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
